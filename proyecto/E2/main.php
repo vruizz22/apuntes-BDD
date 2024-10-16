a2b2623e356cba5ed1302f75e1e7f3c39ba1cdb8
VICTOR RUIZ <veruiz@uc.cl>
2024-10-16 11:00:00 -0300

    coMMIT ACTUAL

--- proyecto/E2/main.php
+++ proyecto/E2/main.php
@@ -1,8 +1,10 @@
 <?php
+ini_set('memory_limit', '10G'); // Aumentar el límite de memoria a 10GB
 require_once 'Cargador.php';
 require_once 'Corrector.php';
 
-$env_string = "host=localhost port=5432 dbname=grupo15 user=grupo15 password=Elefante$15";
+// Cadena de conexión
+$env_string = "host=localhost port=5432 dbname=postgres user=postgres password=Elefante$15";
 
 // Llamada al módulo Cargador
 $cargador = new Cargador($env_string);
